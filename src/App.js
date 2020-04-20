14906e50d9e0335bebdf5bf48b925fd63eec86fd
IT18112160 <lakshan.bandara9320@gmail.com>
2020-04-20 23:43:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
+import React, {useState} from "react";
 
+function Todo({todo,index}){
+
+    return (<div className='todo'>
+
+        <h4>{index}+{todo.text}</h4>
+        <h4> {todo.completed}</h4>
+
+
+    </div>)
+
+}
+
+
+
+function App(){
+
+    const [todos,setTodo]= useState([
+        {
+            text:'Create a project twith react hooks',
+            completed: false
+        },
+        {
+            text:'do coding ',
+            completed: false
+        },
+        {
+            text:'runt the project',
+            completed: false
+        }
+    ]);
+
+
+    return (<div className='app'>
+
+        {todos.map((todo,index)=>(
+            <Todo todo={todo} index={index} key={index} />
+
+        ))}
+
+
+    </div>)
+
+
+}
+
+
+
+export default App;
